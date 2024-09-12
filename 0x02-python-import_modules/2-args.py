dbf67053787f6b17e7eb73a0496c5f0c8ec84079
lilprof01 <adewumime@gmail.com>
2024-09-12 15:05:08 -0700

    done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_args(argc, argv):
+    argc = len(argv) - 1
+
+    if argc == 1:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, argv[1]))
+    elif argc > 1:
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} arguments:".format(argc))
+
+
+if __name__ = "__main__":
+    print_args(len(sys.argv) - 1, sys.argv)

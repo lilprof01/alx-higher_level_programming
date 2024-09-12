021e0ce15335f136ce1325171c4414d5b0547af8
lilprof01 <adewumime@gmail.com>
2024-09-12 15:17:58 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,14 +7,13 @@ def print_args(argc, argv):
 
     if argc == 1:
         print("{} argument:".format(argc))
-        print("{}: {}".format(argc, argv[1]))
     elif argc > 1:
         print("{} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, argv[i]))
     else:
         print("{} arguments:".format(argc))
 
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
 
 if __name__ == "__main__":
     print_args(len(sys.argv) - 1, sys.argv)

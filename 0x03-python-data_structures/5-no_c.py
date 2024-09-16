ba6948ec4c1667407bc6684fc75d2e1939943e27
lilprof01 <adewumime@gmail.com>
2024-09-15 17:28:33 -0700

    can you c me now? yeah, thats what i thought :p

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,7 @@
 
 def no_c(my_string):
     new_string = ""
-
     for char in my_string:
         if char != "c" and char != "C":
-            new_string += char 
+            new_string += char
     return new_string

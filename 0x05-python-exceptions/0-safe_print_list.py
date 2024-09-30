e48d03f4b2eb659d63a9fe04d68e76da1f2e595f
lilprof01 <adewumime@gmail.com>
2024-09-30 10:47:11 -0700

    first task done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,8 +8,8 @@ def safe_print_list(my_list=[], x=0):
         for j in range(0, x):
             print(my_list[j], end="")
         print()
-    except:
+    except IndexError:
         if x > y:
-          print()
-          return y
+            print()
+            return y
     return x

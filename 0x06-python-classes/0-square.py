1305c25f8fef71fe304b6a76f251f8e0f1c09f3b
lilprof01 <adewumime@gmail.com>
2024-10-01 14:33:21 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''0-square.py
+    this is a simple class that defines a square
+'''
+
+
+class Square:
+    '''
+        this is an empty square class definition
+    '''
+    pass

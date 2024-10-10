dae66ed65c8acbb987e2ed27f6a42ff9e8e78dbd
lilprof01 <adewumime@gmail.com>
2024-10-09 17:18:57 -0700

    done

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -90,9 +90,9 @@ class Rectangle:
             return ""
         for i in range(self.__height):
             if i == self.__height - 1:
-                rectangle_str += str(print_symbol * self.__width)
+                rectangle_str += str(print_symbol) * self.__width
             else:
-                rectangle_str += str(print_symbol * self.__width) + "\n"
+                rectangle_str += str(print_symbol) * self.__width + "\n"
         return rectangle_str
 
     def __repr__(self):

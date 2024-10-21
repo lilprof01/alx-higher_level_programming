4f4aa47430f2076b9d5081b33d2849ff2d17d1d0
lilprof01 <adewumime@gmail.com>
2024-10-21 15:28:06 -0700

    fixed again

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
         '''
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -45,7 +45,7 @@ class Rectangle(Base):
         '''
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -61,7 +61,7 @@ class Rectangle(Base):
         """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -77,7 +77,7 @@ class Rectangle(Base):
         """
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
@@ -94,10 +94,10 @@ class Rectangle(Base):
             print("{:s}{:s}".format(" " * self.__x, "#" * self.__width))
 
     def __str__(self):
-        """function displays string rep of rectangle
-        """
-        return f"[Rectangle] ({self.__id}) {self.__x}/{self.__y} \
-                 - {self.__width}/{self.__height}"
+        """Prints rectangle"""
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
+                format(self.id, self.__x, self.__y, self.__width,
+                       self.__height))
 
     def update(self, *args, **kwargs):
         """function updates attributes of rectagle with args

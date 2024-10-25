2fb86b8d85a0751061547b20e15d5f3f0898fc8e
lilprof01 <adewumime@gmail.com>
2024-10-25 11:45:59 -0700

    test in progress

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -24,21 +24,41 @@ class TestBase(unittest.TestCase):
         '''Tests if nb_objects is a private class attribute'''
         self.assertTrue(hasattr(Base, "_Base__nb_objects"))
 
-    def test_initialization(self):
-        '''Tests if nb_objects initializes to zero on setup'''
-        self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
+    #def test_initialization(self):
+        #'''Tests if nb_objects initializes to zero on setup'''
+        #self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
 
-    def test_instance_without_setId(self):
-        '''Tests instance without a set id'''
-        a = Base()
-        b = Base()
-        self.assertEqual(a.id, 1)
-        self.assertEqual(b.id, 2)
+    #def test_instance_without_setId(self):
+        #'''Tests instance without a set id'''
+        #a = Base()
+        #b = Base()
+        #self.assertEqual(a.id, 1)
+        #self.assertEqual(b.id, 2)
 
     def test_instance_with_setId(self):
         '''tests for an instance with a set id'''
         a = Base(15)
         self.assertEqual(a.id, 15)
+
+    def test_continous_ids(self):
+        """ Tests for continous ids in each new instance"""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
+
+    def test_0_id(self):
+        """ Test id to see if it duplicates """
+        Base._Base__nb_objects = 0
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
 
 
 if __name__ == "__main__":

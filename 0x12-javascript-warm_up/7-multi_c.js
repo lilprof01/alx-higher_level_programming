8e16934fa3902d74ed272a8fffcb77bb9c071601
lilprof01 <adewumime@gmail.com>
2024-12-16 12:28:34 -0800

    fixes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2]);
+let arg = parseInt(process.argv[2]);
 
 if (isNaN(arg)) {
   console.log('Missing number of occurrences');
 } else {
-  x = parseInt(arg);
-  while (x > 0) {
+  while (arg > 0) {
     console.log('C is fun');
-    x--;
-   }
+    arg--;
+  }
 }
